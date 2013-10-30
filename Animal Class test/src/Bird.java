d9b2717f70ead354f2a070abc275030920f34a52
Henry <henrygersh@me.com>
2013-10-29 17:56:03 -0700

    Working system of game related classes

---
+++ Animal Class test/src/Bird.java
@@ -0,0 +1,38 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Bird extends Animal {
+	public Bird() {
+// stat set for bird
+		type="Bird";
+		name="Bird";
+		lvl=1;
+		expTot=0;
+		expErnd=0;
+		expToLvl=900;
+		hpTot=750;
+		hpRem=750;
+		apTot=100;
+		apRem=100;
+		att=120;
+		def=100;
+		evd=300;	
+//set up attacks for bird
+		attacksAvail=new ArrayList<Attack>();
+		Peck Attack0 = new Peck();
+		attacksAvail.add(Attack0);
+
+//lvl up statistics for bird
+		hpScaler=11;
+		hpBonus=0;
+		apScaler=10;
+		apBonus= 0;
+		attScaler= 10;
+		attBonus= 0;
+		defScaler=10;
+		defBonus=.2;
+		evdScaler=7.5;
+		evdBonus=.45;	
+	}
+	
+}
---
+++ Animal Class test/src/Peck.java
@@ -0,0 +1,12 @@
+
+public class Peck extends Attack{
+	
+Peck(){
+	this.setName("Peck");	
+	this.setDmg(50);
+	this.setConsist(.95);
+	this.setAcc(.98);
+	this.setCritHitChance(.07);
+	this.setApCost(20);	
+}
+}
